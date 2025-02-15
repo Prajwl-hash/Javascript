e98b33c90ed14ab1525702732f718702b55aa710
Prajwl-hash <prajwlram@gmail.com>
2025-02-15 14:06:16 +0530

    Add files via upload

---
+++ JavaScript/part1/print.js
@@ -0,0 +1,12 @@
+// printing with normal method 
+// this consist built in new line
+console.log("hey prajwal");
+console.log("hey prajwal");
+
+//To print tablish
+console.table({name:"prajwal",age:20});
+
+// printing with process.stdout.write
+// this does not consist built in new line and not uses in normal use.
+process.stdout.write("hey prajwal");
+process.stdout.write("hey prajwal");
